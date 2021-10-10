dd1dd3bb8f2869f0249c7d179ed9b33fe8e026e4
Daniel <dinganadaniel@gmail.com>
2021-10-10 18:55:50 -0500

    First commit

---
+++ c3-s3bucket.tf
@@ -0,0 +1,20 @@
+# Call our Custom Terraform Module which we built earlier
+module "website_s3_bucket123" {
+  source  = "app.terraform.io/TF_Cloud_Demo/s3-website/aws"
+  version = "1.0.0"
+  # insert required variables here
+  bucket_name = var.my_s3_bucket
+  tags = var.my_s3_tags
+}
+
+
+
+/*
+you can use the terraform login command to get credentials and 
+automatically save them in the CLI configuration. If not, you can manually write credentials blocks
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "xxxxxx.atlasv1.zzzzzzzzzzzzz"
+  }
+
+  */
